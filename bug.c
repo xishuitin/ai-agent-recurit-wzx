90d73b7b04ba88c0f038af73c9e157307284e4ea
xishuitin <1700114203@qq.com>
2026-09-20 18:52:58 +0800

    Fix average calculation

--- bug.c
+++ bug.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 double average(double a, double b)
 {
-    return a + b / 2;
+    return (a + b) / 2;
 }
+
 int main()
 {
-    double x = 4, y=6;
+    double x = 4, y = 6;
     double res = average(x, y);
     printf("平均值是：%f\n", res);
     return 0;

12ca5b2b82f6989859d19871ef20d77c9d832af3
xishuitin <1700114203@qq.com>
2026-09-20 18:27:48 +0800

    Create bug.c

---
+++ bug.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+double average(double a, double b)
+{
+    return a + b / 2;
+}
+int main()
+{
+    double x = 4, y=6;
+    double res = average(x, y);
+    printf("平均值是：%f\n", res);
+    return 0;
+}
